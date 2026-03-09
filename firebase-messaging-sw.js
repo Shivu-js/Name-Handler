2a184a9a7b46899378ca186ace892941ef5901d4
Shivu-js <sushil912031@gmail.com>
2026-03-09 09:08:00 +0530

    Add Firebase messaging service worker

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,8 @@
+importScripts("https://www.gstatic.com/firebasejs/12.10.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.10.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  messagingSenderId: "909038043802"
+});
+
+const messaging = firebase.messaging();
